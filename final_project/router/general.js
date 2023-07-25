1f0515b1b522a017c1b5415d73fc2cde2b252cbd
Julia <julialacerdaff@gmail.com>
2023-07-25 19:58:16 -0300

    task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,11 +32,14 @@ public_users.get('/',async (req, res) => {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn
-    res.send(JSON.stringify(books[isbn], null, 4))
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn',async (req, res) => {
+  try {
+      const isbn = await req.params.isbn
+      res.send(JSON.stringify(books[isbn], null, 4))
+  } catch (err) {
+      res.status(500).json({message: "Internal Server Error"})
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
